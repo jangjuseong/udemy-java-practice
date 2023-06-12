cc3274f60e8a335b67eaf3943be91e229aa316c1
juseong <jjs7280@gmail.com>
2023-06-13 01:14:34 +0900

    ~section8-step06

---
+++ introductionJavaProgramming/src/com/in28minutes/oops/Book.java
@@ -0,0 +1,8 @@
+package com.in28minutes.oops;
+
+public class Book {
+	void books() {
+		System.out.println("북 인스턴스 생성");
+	}
+
+}
---
+++ introductionJavaProgramming/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author jangjuseong
+ *
+ */
+module introductionJavaProgramming {
+}
