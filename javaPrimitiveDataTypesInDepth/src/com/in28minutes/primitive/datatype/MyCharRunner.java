50242528d0d6e14c40cc79e450506e49c58874fc
juseong <jjs7280@gmail.com>
2023-06-15 23:22:40 +0900

    section9 step08~step14 boolean, char 자료형

---
+++ javaPrimitiveDataTypesInDepth/src/com/in28minutes/primitive/datatype/MyCharRunner.java
@@ -0,0 +1,11 @@
+package com.in28minutes.primitive.datatype;
+
+public class MyCharRunner {
+
+	public static void main(String[] args) {
+		MyChar myChar = new MyChar('c');
+		System.out.println(myChar.isVowel());
+		
+	}
+
+}
